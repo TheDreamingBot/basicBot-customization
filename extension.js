2283d885aaed76d30445e1247b1ea54902d955ec
TheDreamingBot <TheDreamingBot@users.noreply.github.com>
2015-02-03 18:09:17 +0100

    Update extension.js

--- extension.js
+++ extension.js
@@ -45,6 +45,19 @@
             }
         };
 
+bot.commands.scCommand = {
+            command: 'sc',  //The command to be called. With the standard command literal this would be: !bacon
+            rank: 'user', //Minimum user permission to use the command
+            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
+            functionality: function (chat, cmd) {
+                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                if (!bot.commands.executable(this.rank, chat)) return void (0);
+                else {
+                    API.sendChat("/me Bacon!!!");
+                }
+            }
+        };
+
         //Load the chat package again to account for any changes
         bot.loadChat();
 
@@ -93,6 +106,7 @@
         themeLink: null,
         fbLink: "https://www.facebook.com/DJExlips",
         youtubeLink: "https://www.youtube.com/user/djexlips/",
+        soundcloudLink: "https://soundcloud.com/djexlips"
         website: null,
         intervalMessages: [],
         messageInterval: 5,
